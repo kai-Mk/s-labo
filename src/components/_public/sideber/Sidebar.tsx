efa6a85560e547cecdf616fc39bba2fb53aac39f
kaitomizuuchi <kaitoshuinei42@gmail.com>
2025-02-02 19:56:37 +0900

    サイドバーの表示を制御

--- src/components/_public/sideber/Sidebar.tsx
+++ src/components/_public/sideber/Sidebar.tsx
@@ -1,14 +1,29 @@
+'use client';
+
 import React from 'react';
 import Link from 'next/link';
+import { useTeamMemberData } from '@/hooks/teamMember/useTeamMemberData';
 import AddCircleIcon from '@mui/icons-material/AddCircleOutline';
 import { IconButton } from '@mui/material';
+import { useSession } from 'next-auth/react';
 import styles from './sidebar.module.scss';
 
 const Sidebar = () => {
+  // todo: チームメンバーからチーム名を取得するように変更する必要がある。
+  const { data: session, status } = useSession();
+  const userId = session?.user?.id;
+  const { data } = useTeamMemberData(
+    status === 'authenticated' ? userId : undefined,
+  );
+
   return (
     <div className={styles.sidebar}>
-      <div className={styles.sidebar_logo}></div>
-      <div className={styles.sidebar_logo}></div>
+      {data &&
+        data.length !== 0 &&
+        data.map((item) => (
+          <div className={styles.sidebar_logo} key={item.team_member_id}></div>
+        ))}
+
       <Link href="/team/create">
         <IconButton className={styles.sidebar_add_button}>
           <AddCircleIcon sx={{ width: '50px', height: '50px' }} />
