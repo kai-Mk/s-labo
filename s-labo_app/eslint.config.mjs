f78dee5df718b5706f377d38616e8e1a2fc7a27d
kaitomizuchi <kaitoshuinei42@gmail.com>
2025-02-20 10:55:35 +0000

    開発環境セットアップ

--- s-labo_app/eslint.config.mjs
+++ s-labo_app/eslint.config.mjs
@@ -1,6 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import importPlugin from 'eslint-plugin-import';
+import unicorn from 'eslint-plugin-unicorn';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +14,71 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    plugins: {
+      '@typescript-eslint': typescript,
+      unicorn: unicorn,
+      import: importPlugin,
+    },
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: true,
+      },
+    },
+    rules: {
+      ...(typescript.configs?.['recommended-type-checked']?.rules ?? {}),
+      ...(typescript.configs?.['stylistic-type-checked']?.rules ?? {}),
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        {
+          prefer: 'type-imports',
+          fixStyle: 'inline-type-imports',
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/require-await': 'off',
+      'unicorn/filename-case': [
+        'error',
+        {
+          cases: {
+            camelCase: true,
+            pascalCase: true,
+          },
+          multipleFileExtensions: true,
+        },
+      ],
+      'import/no-default-export': 'error',
+      'func-style': ['error', 'expression', { allowArrowFunctions: true }],
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+    },
+  },
+  {
+    files: ['**/*.tsx'],
+    rules: {
+      'import/no-default-export': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+  {
+    files: [
+      '**/page.tsx',
+      '**/layout.tsx',
+      'next.config.ts',
+      'postcss.config.mjs',
+      'tailwind.config.ts',
+    ],
+    rules: {
+      'import/no-default-export': 'off',
+      'import/prefer-default-export': 'error',
+    },
+  },
 ];
 
 export default eslintConfig;
