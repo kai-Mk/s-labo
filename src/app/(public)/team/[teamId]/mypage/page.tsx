114d7e520a28fe3bafd4e603af24c971a8d011ba
kaitomizuchi <kaitoshuinei42@gmail.com>
2025-03-11 21:19:22 +0900

    マイページレイアウト作成

--- src/app/(public)/team/[teamId]/mypage/page.tsx
+++ src/app/(public)/team/[teamId]/mypage/page.tsx
@@ -2,7 +2,21 @@ import React from 'react';
 import styles from './mypage.module.scss';
 
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <div className={styles.team_mypage}>
+      {/* 退勤とtodo */}
+      <div className={styles.team_mypage_left}>
+        <div className={styles.mypage_todo}>todoリスト</div>
+        <div className={styles.team_mypage_left_line} />
+        <div className={styles.mypage_leaving_report}>退勤報告</div>
+      </div>
+      <div className={styles.team_mypage_center_line} />
+      {/* タイムボクシング */}
+      <div className={styles.team_mypage_right}>
+        <div className={styles.mypage_time_boxing}>タイムボクシング</div>
+      </div>
+    </div>
+  );
 };
 
 export default MyPage;
