6138fda10bd3c732e647a33ae14f216310968c6e
kaitomizuchi <kaitoshuinei42@gmail.com>
2025-03-13 20:37:01 +0900

    コメントno

--- src/features/mypage/components/TodoContainer.tsx
+++ src/features/mypage/components/TodoContainer.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 import styles from '@/app/(public)/team/[teamId]/mypage/mypage.module.scss';
 import AddIcon from '@mui/icons-material/AddCircleOutlineRounded';
+import CreateIcon from '@mui/icons-material/CreateOutlined';
+import { IconButton } from '@mui/material';
 import AddButton from './Todo/AddButton';
 import TodoItem from './Todo/TodoItem';
 
@@ -17,7 +19,12 @@ const TodoContainer = () => {
       </ul>
       <AddButton />
       <div className={styles.mypage_todo_comment}>
-        <h4 className={styles.comment_box_title}>コメント</h4>
+        <div className={styles.comment_box_head}>
+          <h4 className={styles.comment_box_title}>コメント</h4>
+          <IconButton sx={{ width: '24px', height: '24px' }}>
+            <CreateIcon sx={{ height: '16px', width: '16px' }} />
+          </IconButton>
+        </div>
       </div>
     </div>
   );
