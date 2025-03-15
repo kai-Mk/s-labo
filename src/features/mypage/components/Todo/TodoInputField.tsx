6827e94d8b6f027ee5d4326e7108c564fd602e54
kaito mizuuchi <kaitshuinei42@gmail.com>
2025-03-15 09:37:11 +0000

    セレクトでプロジェクトを選択したときにプロジェクト名を選択できるようにする

--- src/features/mypage/components/Todo/TodoInputField.tsx
+++ src/features/mypage/components/Todo/TodoInputField.tsx
@@ -2,6 +2,7 @@
 
 import type { ProjectData } from '@/types/project';
 import type { TaskCategoryData } from '@/types/taskCategory';
+import type { ChangeEvent } from 'react';
 import React, { useEffect, useRef } from 'react';
 import styles from '@/app/(public)/team/[teamId]/mypage/mypage.module.scss';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -12,11 +13,14 @@ import { z } from 'zod';
 
 const createTodoSchema = z.object({
   todo_description: z.string().min(1, 'タスク内容を入力してください'),
+  // 文字列の数字できた場合にcoerceで自動で数値型に変更
   task_category_id: z.coerce.number(),
+  project_id: z.coerce.number().nullable(),
 });
 
 type CreateTodoData = z.infer<typeof createTodoSchema>;
 
+// プロップスの型定義
 interface TodoInputFieldProps {
   isInputField: boolean;
   taskCategories: TaskCategoryData[];
@@ -28,7 +32,7 @@ const TodoInputField = ({
   taskCategories,
   projects,
 }: TodoInputFieldProps) => {
-  // インプットフィールドにフォーカスさせる
+  // 「タスクを追加する」を押したらインプットフィールドにフォーカスさせる
   const inputRef = useRef<HTMLInputElement>(null);
   useEffect(() => {
     if (isInputField && inputRef.current) {
@@ -41,13 +45,31 @@ const TodoInputField = ({
     register,
     handleSubmit,
     formState: { errors },
+    watch,
+    setValue,
   } = useForm<CreateTodoData>({
     resolver: zodResolver(createTodoSchema),
     defaultValues: {
       todo_description: '',
       task_category_id: 1,
+      project_id: null,
     },
   });
+
+  // プロジェクトを選択した際にプロジェクトIDもセットする
+  const selectedCategoryId = watch('task_category_id');
+  const handleCategoryChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const selectedOption = e.target.options[e.target.selectedIndex];
+    const categoryId = selectedOption.value;
+    const projectId = selectedOption.getAttribute('id');
+
+    setValue('task_category_id', Number(categoryId));
+    if (selectedCategoryId === 1 && projectId) {
+      setValue('project_id', Number(projectId));
+    } else {
+      setValue('project_id', null); // プロジェクトが不要な場合はリセット
+    }
+  };
 
   const onSubmit = async (data: CreateTodoData) => {
     // TodoのAPI処理
@@ -77,10 +99,12 @@ const TodoInputField = ({
             inputRef.current = e;
           }}
         />
+        {/* TODO:選択されたのがプロジェクトの場合にプロジェクト名が表示されるようにする */}
         <select
           id="category"
           className={styles.todo_select_category}
           {...register('task_category_id')}
+          onChange={handleCategoryChange}
         >
           {/* optionのバリューはカテゴリーIDを指定 */}
           {taskCategories &&
@@ -91,14 +115,14 @@ const TodoInputField = ({
                 <optgroup
                   key={taskItem.task_category_id}
                   label={taskItem.task_category_name}
-                  id={String(taskItem.task_category_id)}
                 >
                   {projects &&
                     projects.length !== 0 &&
                     projects.map((projectItem) => (
                       <option
                         key={projectItem.project_id}
-                        value={projectItem.project_id}
+                        value={taskItem.task_category_id}
+                        id={String(projectItem.project_id)}
                       >
                         {projectItem.project_name}
                       </option>
