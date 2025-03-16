0da1d1536433daaf3db35bc9763e728078cd81aa
kaitomizuchi <kaitoshuinei42@gmail.com>
2025-03-16 19:37:37 +0900

    セレクトボックスの選択表示条件変更

--- src/features/mypage/components/Todo/TodoInputField.tsx
+++ src/features/mypage/components/Todo/TodoInputField.tsx
@@ -13,6 +13,7 @@ import { z } from 'zod';
 
 const createTodoSchema = z.object({
   todo_description: z.string().min(1, 'タスク内容を入力してください'),
+  category_name: z.string(),
   // 文字列の数字できた場合にcoerceで自動で数値型に変更
   task_category_id: z.coerce.number(),
   project_id: z.coerce.number().nullable(),
@@ -41,34 +42,33 @@ const TodoInputField = ({
   }, [isInputField]);
 
   // react-hook-form
+  const FIRST_PROJECT_NAME = projects[0].project_name;
+  const FIRST_PROJECT_ID = projects[0].project_id;
   const {
     register,
     handleSubmit,
     formState: { errors },
-    watch,
     setValue,
   } = useForm<CreateTodoData>({
     resolver: zodResolver(createTodoSchema),
     defaultValues: {
       todo_description: '',
+      category_name: FIRST_PROJECT_NAME,
       task_category_id: 1,
-      project_id: null,
+      project_id: FIRST_PROJECT_ID,
     },
   });
 
   // プロジェクトを選択した際にプロジェクトIDもセットする
-  const selectedCategoryId = watch('task_category_id');
   const handleCategoryChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const selectedOption = e.target.options[e.target.selectedIndex];
-    const categoryId = selectedOption.value;
-    const projectId = selectedOption.getAttribute('id');
+    const option = e.target.options[e.target.selectedIndex];
+    const categoryId = Number(option.getAttribute('data-category-id'));
+    const projectId = option.hasAttribute('data-project-id')
+      ? Number(option.getAttribute('data-project-id'))
+      : null;
 
     setValue('task_category_id', Number(categoryId));
-    if (selectedCategoryId === 1 && projectId) {
-      setValue('project_id', Number(projectId));
-    } else {
-      setValue('project_id', null); // プロジェクトが不要な場合はリセット
-    }
+    setValue('project_id', projectId);
   };
 
   const onSubmit = async (data: CreateTodoData) => {
@@ -103,7 +103,7 @@ const TodoInputField = ({
         <select
           id="category"
           className={styles.todo_select_category}
-          {...register('task_category_id')}
+          {...register('category_name')}
           onChange={handleCategoryChange}
         >
           {/* optionのバリューはカテゴリーIDを指定 */}
@@ -121,8 +121,9 @@ const TodoInputField = ({
                     projects.map((projectItem) => (
                       <option
                         key={projectItem.project_id}
-                        value={`${taskItem.task_category_id}-${projectItem.project_id}`}
-                        id={String(projectItem.project_id)}
+                        value={projectItem.project_name}
+                        data-category-id={taskItem.task_category_id}
+                        data-project-id={projectItem.project_id}
                       >
                         {projectItem.project_name}
                       </option>
@@ -131,7 +132,8 @@ const TodoInputField = ({
               ) : (
                 <option
                   key={taskItem.task_category_id}
-                  value={taskItem.task_category_id}
+                  value={taskItem.task_category_name}
+                  data-category-id={taskItem.task_category_id}
                 >
                   {taskItem.task_category_name}
                 </option>
