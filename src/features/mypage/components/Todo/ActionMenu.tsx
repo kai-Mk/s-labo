ef2e40efc33c089c5e3b0ba9268dedff5f65027c
kaitomizuchi <kaitoshuinei42@gmail.com>
2025-03-22 18:30:34 +0900

    Todoアクションボックスの表示非表示

--- src/features/mypage/components/Todo/ActionMenu.tsx
+++ src/features/mypage/components/Todo/ActionMenu.tsx
@@ -1,18 +1,27 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '@/app/(public)/team/[teamId]/mypage/mypage.module.scss';
 import DehazeIcon from '@mui/icons-material/Dehaze';
 import { IconButton } from '@mui/material';
 
 const ActionMenu = () => {
+  const [isActionMenu, setIsActionMenu] = useState(false);
   return (
-    <>
+    <div className={styles.action_menu}>
       <IconButton>
-        <DehazeIcon className={styles.action_menu_button} />
+        <DehazeIcon
+          className={styles.action_menu_button}
+          onClick={() => setIsActionMenu(!isActionMenu)}
+        />
       </IconButton>
-      <div className={styles.action_menu_box}></div>
-    </>
+      <ul
+        className={`${styles.action_menu_list} ${isActionMenu ? styles.active : ''}`}
+      >
+        <li className={styles.action_menu_item}>編集</li>
+        <li className={styles.action_menu_item}>削除</li>
+      </ul>
+    </div>
   );
 };
 
